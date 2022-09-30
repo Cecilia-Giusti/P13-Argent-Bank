2ac98864ac6983e53f07666b06aa7f82fb64092e
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-09-30 15:09:52 +0200

    Feat : add header component

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
      <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer/>
     </BrowserRouter>
   );
 };
